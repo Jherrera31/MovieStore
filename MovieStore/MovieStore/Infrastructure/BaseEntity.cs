e992692a382d84b0d74c5d2e92a00166de0b0f52
Jefferson Benzan Tapia <Jeffersonbt20@gmail.com>
2017-10-19 11:38:48 -0400

    According to #14 we now have created the BaseEntity Class

---
+++ MovieStore/MovieStore/Infrastructure/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieStore.Infrastructure
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
